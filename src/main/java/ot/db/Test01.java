84fb68d860a9760e633db41e6789929e59009c40
ootabitbucket <oota.sayaka12@gmail.com>
2018-12-05 17:16:18 +0900

    oracle test

--- src/main/java/ot/db/Test01.java
+++ src/main/java/ot/db/Test01.java
@@ -1,5 +1,11 @@
 package ot.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import ot.utils.Basic;
 
 public class Test01 extends Basic{
@@ -7,6 +13,45 @@ public class Test01 extends Basic{
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		out("start");
+		test01();
+		out("end");
 	}
 
+	private static void test01() {
+	     try {
+	         Class.forName("org.postgresql.Driver");
+
+     	     //接続文字列
+		     String url = "jdbc:postgresql://localhost:15433/postgres";
+		     String user = "postgres";
+		     String password = "";
+	         try(Connection conn = DriverManager.getConnection(url, user, password)){
+		         //自動コミットOFF
+		         conn.setAutoCommit(false);
+
+		         //SELECT文の実行
+		         try(Statement stmt = conn.createStatement()){
+			         //INSERT文の実行
+			         String sql = "INSERT INTO t01 VALUES (1)";
+			         stmt.executeUpdate(sql);
+			         conn.commit();
+
+			         sql = "SELECT * FROM t01";
+			         try(ResultSet rset = stmt.executeQuery(sql)){
+				         //SELECT結果の受け取り
+				         while(rset.next()){
+				             String col = rset.getString(1);
+				             System.out.println(col);
+				         }
+			         }
+		         }
+	         }
+	     }
+	     catch (ClassNotFoundException e) {
+	         e.printStackTrace();
+	     }
+	     catch (SQLException e){
+	         e.printStackTrace();
+	     }
+	 }
 }
